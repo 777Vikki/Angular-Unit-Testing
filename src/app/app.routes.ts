b77025324eaa63ebdf9426cd8ad2887784dad2ce
Vivek <vkmm777@gmail.com>
2025-10-26 16:43:51 +0530

    implement input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Parent } from './parent/parent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/parent' },
+    { path: 'parent', component: Parent }
+];
