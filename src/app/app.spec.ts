a78d8e6fb7c2ecbd09ba962b1f31dde456f652fc
Vivek <vkmm777@gmail.com>
2025-11-11 22:29:13 +0530

    add reactive form

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let component: App;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, ReactiveFormsModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges()
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create form with 2 controls', () => {
+    expect(component.userForm?.contains('name')).toBeTrue(); 
+    expect(component.userForm?.contains('email')).toBeTrue(); 
+  }); 
+
+  it('should make name control required', () => {
+    const nameControl = component.userForm?.get('name');
+    nameControl?.setValue('');
+    expect(nameControl?.valid).toBeFalse();
+    expect(nameControl?.errors?.['required']).toBeTrue();
+    nameControl?.setValue('Vivek');
+    expect(nameControl?.valid).toBeTrue();
+    expect(nameControl?.errors?.['required']).toBe(undefined);
+  });
+
+  it('should validate email format', () => {
+    const emailControl = component.userForm?.get('email');
+    emailControl?.setValue('InvalidEmail');
+    expect(emailControl?.valid).toBeFalse();
+    emailControl?.setValue('vivek@vk.com');
+    expect(emailControl?.valid).toBeTrue();
+  });
+
+  it('should make form valid when all fields are correct', () => {
+    component.userForm?.setValue({
+      name: 'Vivek',
+      email: 'vivek@vk.com',
+    });
+    expect(component.userForm?.valid).toBeTrue();
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  userForm: FormGroup | undefined;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.createUserForm();
+  }
+
+  createUserForm(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
 }
